18738ad2a4f3c00db2cfe252265872a349a77171
Olukpe <viakinnou.olukpe@gmail.com>
2022-11-30 19:27:41 -0500

    recent changes

--- pages/django_project/settings.py
+++ pages/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9nj$y52p7dvfbl(_$4l0_n+(um_37n5pi&l+scag!fe&rkw^m@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
